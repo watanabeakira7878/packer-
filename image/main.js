9b40b4bffd1fcdd4abf30029cb7d7412ba9d373e
watanabeakira <oxgame7878dayo@icloud.com>
2019-08-16 18:20:11 +0900

    2waytote.html作成

---
+++ image/main.js
@@ -0,0 +1,25 @@
+"use strict"
+
+/* ---product shop ---*/
+
+$(function(){
+
+    $(".sub1").on("click",function(){
+       $(".main-veiw").children("img").css("src", "image/tote-twoway1.jpg");
+    });
+
+    $(".sub2").on("click",function(){
+        $(".main-veiw").children("img").css("src", "image/tote-twoway3.jpg");
+     });
+
+     $(".sub3").on("click",function(){
+        $(".main-veiw").children("img").css("src", "image/tote-twoway4.jpg");
+     });
+
+     $(".sub4").on("click",function(){
+        $(".main-veiw").children("img").css("src", "image/tote-twoway5.jpg");
+     });
+
+    
+
+});
